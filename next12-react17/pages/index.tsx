0f270e171222121af8258d92320f93d7c6c735b9
Thomas Dubois <thomas.dubois@rednet.io>
2023-01-12 10:18:33 +0100

    init commit

---
+++ next12-react17/pages/index.tsx
@@ -0,0 +1,7 @@
+export const getServerSideProps = () => {
+  return { props: {} };
+};
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return null;
+}
